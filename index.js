b55066b61e8c9bc57394114cc60c024cdd2708fc
Robson Melo de Souza <sys.robson@gmail.com>
2023-10-28 16:35:53 -0300

    added shades generation for info, success, warning and background

--- index.js
+++ index.js
@@ -2,25 +2,30 @@ const chroma = require('chroma-js');
 
 const generateShades = (color, isDark = false) => {
   const shades = {};
-  shades[500] = color; // The passed color is the midpoint
+  shades[500] = color;
+  shades.light = chroma(color).brighten(0.5).hex();
+  shades.dark = chroma(color).darken(0.5).hex();
+  shades.contrastText = isDark ? '#ffffff' : '#000000';
 
   for (let i = 1; i <= 4; i++) {
-    const lighterShade = i * 100;
-    const darkerShade = 600 + (i * 100);
-    const factor = isDark ? (4 - i) : i; // Invert the shades for dark mode
+    const lighterShade = 500 - (i * 100);
+    const darkerShade = 500 + (i * 100);
+    const factor = i;
 
-    // For light mode: 100-400 are darker, 600-900 are lighter
-    // For dark mode: 100-400 are lighter, 600-900 are darker
-    shades[lighterShade] = chroma(color).darken(isDark ? factor * 0.5 : -factor * 0.5).hex();
-    shades[darkerShade] = chroma(color).darken(isDark ? -factor * 0.5 : factor * 0.5).hex();
+    shades[lighterShade] = chroma(color).brighten(isDark ? -factor * 0.5 : factor * 0.5).hex();
+    shades[darkerShade] = chroma(color).brighten(isDark ? factor * 0.5 : -factor * 0.5).hex();
   }
 
   return shades;
 };
 
-const generateTheme = (primaryColor, secondaryColor, mode) => {
+const generateTheme = ({primaryColor, secondaryColor, mode, warning, error, success, info, background}) => {
   const primaryShades = generateShades(primaryColor || '#3f50b5', mode === 'dark');
   const secondaryShades = generateShades(secondaryColor || '#f44336', mode === 'dark');
+  const warningShades = generateShades(warning || '#ff9800', mode === 'dark');
+  const errorShades = generateShades(error || '#f44336', mode === 'dark');
+  const successShades = generateShades(success || '#4caf50', mode === 'dark');
+  const infoShades = generateShades(info || '#2196f3', mode === 'dark');
 
   return {
     palette: {
@@ -33,9 +38,31 @@ const generateTheme = (primaryColor, secondaryColor, mode) => {
         ...secondaryShades,
         main: secondaryShades[500]
       },
+      warning: {
+        ...warningShades,
+        main: warningShades[500]
+      },
+      error: {
+        ...errorShades,
+        main: errorShades[500]
+      },
+      success: {
+        ...successShades,
+        main: successShades[500]
+      },
+      info: {
+        ...infoShades,
+        main: infoShades[500]
+      },
+      background: {
+        default: background || '#fafafa',
+        paper: chroma(background || '#fafafa').darken(0.1).hex()
+      },
       contrastText: mode === 'light' ? '#000000' : '#ffffff'
     }
   };
 };
 
 module.exports = { generateTheme };
+
+
